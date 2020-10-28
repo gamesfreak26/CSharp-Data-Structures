486ffa2ca3f6ff71f0e61a98a74957811b472eb0
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2020-10-28 22:54:12 +1000

    Write Unit Tests

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using DataStucturesCSharp.DataStructures;
 using NUnit.Framework;
 using Shouldly;
@@ -5,28 +6,75 @@ using Shouldly;
 namespace TestProject {
     public class Tests {
 
-        
+
         [SetUp]
-        public void Setup() {
-        }
+        public void Setup() { }
 
         [Test]
         public void AddItemToList() {
-            var _list = new GenericList<int> {
+            var list = new GenericList<int> {
                 0
             };
-            var numberOfElements = _list.Count;
 
-            numberOfElements.ShouldBe(1);
+            list.ShouldNotBeEmpty();
+            list.Count.ShouldBe(1);
         }
 
         [Test]
         public void AddMoreItemsThanCapacityToList() {
-            GenericList<int> _list = new GenericList<int>();
-            for (var i = 0; i < 15; i++) {
-                _list.Add(i);
-            } 
-            _list.Count.ShouldBe(15);
+            var numberOfItemsToInsert = 11;
+            var list = new GenericList<int>(10);
+
+            for (var i = 0; i < numberOfItemsToInsert; i++) {
+                list.Add(i);
+            }
+
+            list.ShouldNotBeEmpty();
+            list.Count.ShouldBe(numberOfItemsToInsert);
+        }
+
+        [Test]
+        public void RemoveElement() {
+            var list = new GenericList<int> {
+                0
+            };
+
+            list.RemoveAt(0);
+            
+            list.ShouldBeEmpty();
+            list.Count.ShouldBe(0);
+        }
+
+        [Test]
+        public void CheckIsEmpty() {
+            var list = new GenericList<int>();
+
+            var empty = list.IsEmpty();
+
+            empty.ShouldBeTrue();
+        }
+
+        [Test]
+        public void CheckIsEmptyWhenNotEmpty() {
+            var list = new GenericList<int> {
+                0
+            };
+
+            var empty = list.IsEmpty();
+
+            empty.ShouldBeFalse();
+        }
+
+        [Test]
+        public void ClearList() {
+            var list = new GenericList<int> {
+                0, 
+                1
+            };
+
+            list.Clear();
+
+            list.Count.ShouldBe(0);
         }
     }
 }
